b01dd0e44c5ab474b4c4f55337243bec3ea1b1e6
Denis Krivostchokov <densomart@gmail.com>
2014-01-31 21:43:44 +0700

    generate authors controller

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,49 @@
+class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @authors = Author.all
+  end
+
+  def show
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def edit
+  end
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to @author, notice: 'Author was successfully created.'
+    else
+      render action: 'new' 
+    end
+  end
+
+  def update
+    if @author.update(author_params)
+      redirect_to @author, notice: 'Author was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @author.destroy
+    redirect_to authors_url
+  end
+
+  private
+    def set_author
+      @author = Author.find(params[:id])
+    end
+
+    def author_params
+      params.require(:author).permit(:name, :description, :short_description)
+    end
+end
