9a0735a9a43b1e08c4d21bda00b73a0b5caa657e
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-02-15 23:48:22 +0400

    cleanup_pry

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,2 @@
 class RegistrationsController < Devise::RegistrationsController
-  def sign_up_params
-    params = devise_parameter_sanitizer.sanitize(:sign_up)
-
-    binding.pry
-
-    params
-  end
 end
