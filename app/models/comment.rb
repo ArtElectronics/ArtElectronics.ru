53d375d8ebba4c34082db5e0e1fae5471a6c2cc6
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-02-16 18:24:56 +0400

    views

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   include TheComments::Comment
   include CommonClassMethods
+
+  def self.recent_five
+    with_state(:published).where(commentable_state: :published).recent.limit(5)
+  end
 end
