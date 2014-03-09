a4b5998e22690774e6159506d733c4f46a1f8958
Ilya N. Zykin <zykin-ilya@ya.ru>
2014-03-10 01:40:18 +0400

    future_text

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,16 @@ class Post < ActiveRecord::Base
   # relations
   has_many :authorships
   has_many :authors, through: :authorships
+
+  def future_type
+    "application/x-shockwave-flash"
+  end
+
+  def future_text?
+    attached_files.map(&:attachment_content_type).include? future_type
+  end
+
+  def future_text
+    attached_files.where(attachment_content_type: future_type).first.url
+  end
 end
