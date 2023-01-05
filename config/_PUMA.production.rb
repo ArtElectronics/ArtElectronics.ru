5d4498a59ce4583aba53d8d121bf382eb96295bf
Ilya N. Zykin <zykin-ilya@ya.ru>
2023-01-05 22:16:54 +0300

    update 2023

--- config/_PUMA.production.rb
+++ config/_PUMA.production.rb
@@ -3,14 +3,14 @@ threads 1, 4
 daemonize true
 environment "production"
 
-bind "unix:///home/tmp/sockets/puma.sock"
+bind "unix:///home/lucky/app/tmp/sockets/puma.sock"
 
-pidfile    "/home/tmp/pids/puma.pid"
-state_path "/home/tmp/pids/puma.state"
+pidfile    "/home/lucky/app/tmp/pids/puma.pid"
+state_path "/home/lucky/app/tmp/pids/puma.state"
 
 stdout_redirect \
-  "/home/log/puma.log",
-  "/home/log/puma.errors.log",
+  "/home/lucky/app/log/puma.log",
+  "/home/lucky/app/log/puma.errors.log",
   true
 
 activate_control_app
@@ -18,5 +18,5 @@ activate_control_app
 on_worker_boot do
   require "active_record"
   ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("/home/config/database.yml")["production"])
+  ActiveRecord::Base.establish_connection(YAML.load_file("/home/lucky/app/config/database.yml")["production"])
 end
